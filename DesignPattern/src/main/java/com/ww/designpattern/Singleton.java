48034d950333883ca428083d8283c8a2ec208db5
weiwei <weiwei@zhizhangyi.com>
2022-12-26 17:51:46 +0800

    first commit

---
+++ DesignPattern/src/main/java/com/ww/designpattern/Singleton.java
@@ -0,0 +1,9 @@
+package com.ww.designpattern;
+
+/**
+ * @Author weiwei
+ * @Date 2022/12/26 16:39
+ */
+public class Singleton {
+
+}
---
+++ DesignPattern/src/main/java/com/ww/designpattern/Test.java
@@ -0,0 +1,12 @@
+package com.ww.designpattern;
+
+/**
+ * @Author weiwei
+ * @Date 2022/12/26 16:51
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hhhhh");
+    }
+}
